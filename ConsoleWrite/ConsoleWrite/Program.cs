3bdc349a4992ee041c7cd7ac1a84662149b5d405
leeyunhome <leeyunhome@daum.net>
2020-03-31 09:04:44 +0900

    add MyFirstProgram, ConsoleWrite

---
+++ ConsoleWrite/ConsoleWrite/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace ConsoleWrite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Write");
+            Console.WriteLine("WriteLine을 쓰면");
+        }
+    }
+}
