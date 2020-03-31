65f94f4ee7289d7cc2483af4cdca3dc3048490c6
leeyunhome <leeyunhome@daum.net>
2020-03-31 09:50:42 +0900

    add Echo

--- UltimateBaseball/UltimateBaseball/Program.cs
+++ UltimateBaseball/UltimateBaseball/Program.cs
@@ -9,6 +9,14 @@ namespace UltimateBaseball
         static void Main(string[] args)
         {
             Console.WriteLine("숫자야구게임");
+            Console.WriteLine("------------------------------------------------------");
+            Console.WriteLine("> 수비수가 고른 숫자 ");
+            int number1 = 3;
+            int number2 = 1;
+            int number3 = 9;
+            Console.WriteLine(number1);
+            Console.WriteLine(number2);
+            Console.WriteLine(number3);
         }
     }
 }
---
+++ Variable/Variable/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Variable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int appleCount = 12;
+            int pearCount;
+            Console.WriteLine(appleCount);
+        }
+    }
+}
