ca70306c6ebb55bbae4f135bbe4e0b6d9fdd61c2
leeyunhome <leeyunhome@daum.net>
2020-03-31 10:01:40 +0900

    add Echo, Calculator

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("첫 번째 숫자를 입력하세요");
+            string userInput1 = Console.ReadLine();
+            double number1 = double.Parse(userInput1);
+
+            Console.WriteLine(number1 + 1);
+        }
+    }
+}
